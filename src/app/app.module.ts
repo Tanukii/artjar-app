c11a407c2482ecf25620a89ab06369bac219668f
rbi25739 <rbi25739@zwoho.com>
2021-05-20 19:37:55 +0200

    Incluida Libreria con Exito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { bootstrap } from 'bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/Layout/app.component';
+import { IndexComponentComponent } from './components/IndexComponent/index-component.component';
+import { LogSignComponent } from './components/Log-SignComponent/log-sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponentComponent,
+    LogSignComponent
   ],
   imports: [
     BrowserModule,
