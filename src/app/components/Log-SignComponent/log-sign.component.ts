a6a9b29a51e120ff9871e4fa438142d0fed73642
rbi25739 <rbi25739@zwoho.com>
2021-05-24 21:55:53 +0200

    Implementacion Parcial LogRegistro

--- src/app/components/Log-SignComponent/log-sign.component.ts
+++ src/app/components/Log-SignComponent/log-sign.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-log-sign',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogSignComponent implements OnInit {
 
-  constructor() { }
+  // - Propiedades FormGroup -
+  public formLogin: FormGroup;
+  public formRegistro: FormGroup;
 
+  constructor() {
+    // v --- FORM GROUP LOGIN --- v
+    this.formLogin = new FormGroup(
+      {
+        nickname: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12)
+        ]),
+        contra: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12)
+        ])
+      }
+    );
+    // ^ --- FORM GROUP LOGIN --- ^
+
+    // v --- FORM GROUP REGISTRO --- v
+    this.formRegistro = new FormGroup(
+      {
+        nickname: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12)
+        ]),
+        contra: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12)
+        ]),
+        recontra: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12)
+        ])
+
+      }, this._testPassword()
+    );
+    // ^ --- FORM GROUP REGISTRO --- ^
+  }
+
+  // --- METODO ON INIT ---
   ngOnInit(): void {
   }
+
+  // --- METODO TEST PASSWORD ---
+  private _testPassword():ValidatorFn{
+
+    return (fg: FormGroup): ValidationErrors => {
+
+      var _vContra=fg.controls['contra'].value;
+      var _vReContra=fg.controls['recontra'].value;
+  
+      if (_vContra != _vReContra) {
+          fg.controls['recontra'].setErrors({ confirmIgualPassword: false });
+      } else {
+          fg.controls['recontra'].setErrors(null);
+      }
+      return null;
+    }
+  }
+
+  // --- METODO TEST NICKNAME ---
+
+
+  // --- Metodo LOGIN ---
+
+
+  // --- METODO REGISTRO ---
+  
+  
 
 }
