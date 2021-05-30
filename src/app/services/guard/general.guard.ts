04635736d746399378f7d2f4d6509dd3a1d0af4a
rbi25739 <rbi25739@zwoho.com>
2021-05-30 20:39:48 +0200

    Guard borra token si caducado

--- src/app/services/guard/general.guard.ts
+++ src/app/services/guard/general.guard.ts
@@ -28,6 +28,9 @@ export class GeneralGuard implements CanActivate {
       if(_boolTest.res){
         return true;
       } else {
+        if(_boolTest.exp){
+          this._tokenStore.setTokenFromREST(null);
+        }
         this._router.navigate(['LoginRegistro']);
       }
       
