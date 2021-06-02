6fe8518a9e8c385736c5aac30df1c6a9b04dfd0c
rbi25739 <rbi25739@zwoho.com>
2021-06-02 20:55:47 +0200

    Implementado formulario subida imagen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './components/Layout/app.component';
 import { IndexComponent } from './components/IndexComponent/index-component.component';
 import { LogSignComponent } from './components/Log-SignComponent/log-sign.component';
 import { Error404Component } from './components/error404/error404.component';
+import { SubirImagenesComponent } from './components/subir-imagenes-component/subir-imagenes-component.component';
 
 // --- Servicios a Inyectar ---
 import { LogSignService } from './services/http/LogSign.service';
 import { SessionVarService } from './services/session/session-var.service';
+import { FileUploadService } from './services/http/file-upload.service';
 import { GeneralGuard } from 'src/app/services/guard/general.guard';
-import { SubirImagenesComponent } from './components/subir-imagenes-component/subir-imagenes-component.component';
+
 
 
 @NgModule({
@@ -35,7 +38,8 @@ import { SubirImagenesComponent } from './components/subir-imagenes-component/su
   providers: [ 
     LogSignService, 
     SessionVarService,
-    GeneralGuard
+    FileUploadService,
+    GeneralGuard,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/subir-imagenes-component/subir-imagenes-component.component.ts
+++ src/app/components/subir-imagenes-component/subir-imagenes-component.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// - Servicios -
+import { FileUploadService } from 'src/app/services/http/file-upload.service';
+
 
 @Component({
   selector: 'app-subir-imagenes-component',
@@ -6,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubirImagenesComponent implements OnInit {
 
-  constructor() { }
+  // - Propiedades Componente -
+  public formImg: FormGroup;
+  private _fileVar: File;
+
+  constructor(
+    private _fileUpload: FileUploadService,
+    private _router: Router
+  ) { 
+    this.formImg = new FormGroup(
+      {
+        imgInput: new FormControl('', [
+          Validators.required,
+          Validators.pattern('.*\.(jpe?g|bmp|png)$')
+
+        ]),
+        precioInput: new FormControl('', [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(9999),
+          Validators.pattern('^[0-9]{1,}$')
+
+        ])
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
+
+  public cargaFichero(InputImgTag){
+    this._fileVar = InputImgTag.files[0] as File;
+  }
+
+  public suibirImagen(){
+    console.log(this.formImg.value)
+    console.log(this._fileVar);
+    //this._fileUpload.SubirFicheros();
+  }
+
+
 
 }
