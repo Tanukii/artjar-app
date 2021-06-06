db1324ef9cd64276f127dbfb62e52d7ec55c492d
rbi25739 <rbi25739@zwoho.com>
2021-06-06 06:12:49 +0200

    Comunicacion subida terminada, hay fallos

--- src/app/components/subir-imagenes-component/subir-imagenes-component.component.ts
+++ src/app/components/subir-imagenes-component/subir-imagenes-component.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 
 // - Servicios -
 import { FileUploadService } from 'src/app/services/http/file-upload.service';
+import { SessionVarService } from 'src/app/services/session/session-var.service';
 
 
 @Component({
@@ -15,16 +16,18 @@ export class SubirImagenesComponent implements OnInit {
   // - Propiedades Componente -
   public formImg: FormGroup;
   private _fileVar: File;
+  public IconImg: string = "<i class='bi bi-cloud-arrow-up-fill'></i>";
 
   constructor(
     private _fileUpload: FileUploadService,
+    private _tokenStore: SessionVarService,
     private _router: Router
   ) { 
     this.formImg = new FormGroup(
       {
         imgInput: new FormControl('', [
           Validators.required,
-          Validators.pattern('.*\.(jpe?g|bmp|png)$')
+          Validators.pattern('^.*\.(jpe?g|png)$')
 
         ]),
         precioInput: new FormControl('', [
@@ -46,24 +49,44 @@ export class SubirImagenesComponent implements OnInit {
   }
 
   public async suibirImagen(){
-    console.log('Subiendo Imagen');
+    // - Spinner informativo -
+    this.IconImg = "<span class='spinner-grow spinner-grow-sm text-info'></span>";
+    
+    // - Creacion de paquete de datos -
     let formData = new FormData();
     formData.append('foto', this._fileVar,'MiFoto.jpg');
     formData.append('precio',this.formImg.value.precioInput as string);
+    formData.append('jwt', this._tokenStore.getJwt());
 
     this._fileUpload.SubirFicheros(formData).subscribe(
       (success)=>{
-      if (success.status === 200) {
+      if (success.status === 200 && success.body.exp === false) {
+        // - Si fue con exito se re-enruta -
         this._router.navigate(['Index']);
       } else {
+        // - Nunca deberia entrar por aqui -
+        this.IconImg = "<i class='bi bi-exclamation-triangle-fill text-warning'></i>";
         console.log(success);
       }
       
     },
     (err)=>{
       if (err.status === 400) {
-        console.log(err);
+        if(err.body.exp){
+          // - Token expirado, retornar a Login/Registro -
+          this._tokenStore.setTokenFromREST(null);
+          this._router.navigate(['LoginRegistro']);
+        }else{
+          // - Error en el proceso -
+          this.IconImg = "<i class='bi bi-x-square-fill text-danger'></i>";
+        }
+      }else{
+        // - Otro error distinto en servidor -
+        this.IconImg = "<i class='bi bi-exclamation-triangle-fill text-warning'></i>";
+        
       }
+      console.log(err);
+      
     });
   }
 
